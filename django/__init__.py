aa4a26cb1fff9a2c2aa449633f96cdc3a1419feb
danivan <danushivan@gmail.com>
2020-05-20 21:16:04 +0300

    Initial commit

---
+++ django/__init__.py
@@ -0,0 +1,21 @@
+name1 = 0
+name2 = 1
+
+def go_to_hell():
+    pass
+
+
+def be_yourself():
+    pass
+
+
+def find_your_bugs():
+    pass
+
+
+def be_careful():
+    pass
+
+
+def apple():
+    pass
---
+++ flask/file1.py
@@ -0,0 +1,6 @@
+def calculate_expression():
+    pass
+
+
+def be_funny():
+    pass
